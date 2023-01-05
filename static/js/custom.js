755a66ff4fec69798b7c65e8e160a96bb43b68e7
jiakai <gujiakai28@gmail.com>
2023-01-05 12:31:13 +0800

    change js work area

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("img").each(function() {
+    $("content img").each(function() {
         var currentImage = $(this);
         currentImage.wrap("<a href='" + currentImage.attr("src") + "' data-fancybox='gallery' data-caption='" + currentImage.attr("alt") + "'></a>");
     });
